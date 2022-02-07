6f14b389fed2241efd70d7404fa484c8fd8f717b
krisnafirdaus <krisnafirdaus@gmail.com>
2022-02-07 20:38:00 +0800

    init server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const flash = require("express-flash");
+const { PORT = 3000 } = process.env;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "krisna123",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+const passport = require("./lib/passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("view engine", "ejs");
+
+const router = require("./router");
+app.use(router);
+app.listen(PORT, () => {
+  console.log(`Server nyala di port ${PORT}`);
+});
