fb22a65609567322f4352a96f39917db7660f2cd
krisnafirdaus <krisnafirdaus@gmail.com>
2022-02-09 21:02:06 +0800

    initial passport

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("./controllers/authControllers");
 
 router.get("/", (req, res) => res.render("index"));
+router.get("/login", (req, res) => res.render("login"));
 
 router.get("/register", (req, res) => res.render("register"));
 router.post("/register", auth.register);
