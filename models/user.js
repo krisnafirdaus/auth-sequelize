9f23387b816cba339280e814a9addc742411a2aa
krisnafirdaus <krisnafirdaus@gmail.com>
2022-02-09 20:24:53 +0800

    initial project

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 const bcrypt = require("bcrypt");
+const { password } = require("pg/lib/defaults");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -18,6 +19,20 @@ module.exports = (sequelize, DataTypes) => {
       const encryptedPassword = this.#encrypt(password);
 
       return this.create({ username, password: encryptedPassword });
+    };
+
+    checkPassword = (password) => bcrypt.compareSync(password, this.password);
+
+    static authenticate = async ({ username, password }) => {
+      try {
+        const user = await this.findOne({ where: { username } });
+        if (!user) return Promise.reject("User Not found");
+        const isPasswordValid = user.checkPassword(password);
+        if (!isPasswordValid) return Promise.reject("Password anda salah");
+        return Promise.resolve(user);
+      } catch (err) {
+        return Promise.reject(err);
+      }
     };
   }
   User.init(
